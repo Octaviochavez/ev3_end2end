ac43cd3ef5ea5775ff8d8cc3ad1c026af1f2aaa9
Huinca <huincaman.felipe@gmail.com>
2026-06-23 16:56:10 -0400

    On branch rama_Felipe  Your branch is up to date with 'origin/rama_Felipe'.
     Changes to be committed:
    	modified:   api/train.py

--- api/train.py
+++ api/train.py
@@ -8,10 +8,14 @@ import json
 
 from sqlalchemy import create_engine
 from mpl_toolkits.mplot3d import Axes3D
-
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-
 from sklearn.metrics import silhouette_score
 from sklearn.decomposition import PCA
 from kneed import KneeLocator
+
+os.makedirs("models", exist_ok=True)
+
+usuarios = pd.read_csv("data/usuarios_streaming.csv")
+perfiles = pd.read_csv("data/perfil_usuarios.csv")
+print(usuarios.head())
