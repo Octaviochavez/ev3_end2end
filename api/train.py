a9e34e63d8df89985ffd6be5e16c40016633b9ff
Huinca <huincaman.felipe@gmail.com>
2026-06-23 14:17:46 -0400

    On branch rama_Felipe  Your branch is up to date with 'origin/rama_Felipe'.  Changes to be committed: 	deleted:    api/train.ipynb 	new file:   api/train.py

---
+++ api/train.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+import os
+import json
+
+from sqlalchemy import create_engine
+from mpl_toolkits.mplot3d import Axes3D
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.metrics import silhouette_score
+from sklearn.decomposition import PCA
+from kneed import KneeLocator
