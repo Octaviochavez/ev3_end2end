f2832a2058c63e2b882c0910b210b95b5b14bde1
David Ramírez <davidrrluna@gmail.com>
2026-06-23 23:29:33 -0400

    commit

--- api/train.py
+++ api/train.py
@@ -1,13 +1,9 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pickle
 import os
 import json
 
 from sqlalchemy import create_engine
-from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score
