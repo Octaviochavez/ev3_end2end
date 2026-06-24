4bde79dab8d8a62817b38d0949094f124ce7947a
Huinca <huincaman.felipe@gmail.com>
2026-06-23 22:14:16 -0400

    On branch rama_Felipe  Your branch is up to date with 'origin/rama_Felipe'.
     Changes to be committed:
    	modified:   api/train.py

--- api/train.py
+++ api/train.py
@@ -18,4 +18,64 @@ os.makedirs("models", exist_ok=True)
 
 usuarios = pd.read_csv("data/usuarios_streaming.csv")
 perfiles = pd.read_csv("data/perfil_usuarios.csv")
-print(usuarios.head())
+
+data = pd.merge(usuarios, perfiles, on="id_cliente")
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(data.drop(columns=["id_cliente"]))
+
+inertias = []
+silhouettes = []
+for k in range(2,11):
+    modelo = KMeans(n_clusters=k, random_state=29, n_init=10)
+    modelo.fit(X_scaled)
+
+    inertias.append(modelo.inertia_)
+    silhouettes.append(silhouette_score(X_scaled, modelo.labels_))
+
+kl = KneeLocator(
+    range(2,11),
+    inertias,
+    curve='convex',
+    direction='decreasing'
+)
+
+k_optimo = kl.elbow
+kmeans = KMeans(n_clusters=k_optimo, random_state=29, n_init=10)
+
+clusters = kmeans.fit_predict(X_scaled)
+data["cluster"] = clusters
+
+print("Modelo de segmentación creado con éxito. Número óptimo de clusters:", k_optimo)
+
+pca = PCA(n_components=2)
+componentes = pca.fit_transform(X_scaled)
+data["pc1"] = componentes[:, 0]
+data["pc2"] = componentes[:, 1]
+
+data.to_csv("data/clientes_segmentados.csv", index=False)
+
+metricas = {
+    "k_optimo": int(k_optimo),
+    "silhouette_score": silhouette_score(X_scaled, data["cluster"]),
+    "n_clientes": int(len(data)),
+    "n_clusters": int(k_optimo),
+    "varianza_pca": float(
+        pca.explained_variance_ratio_.sum()
+    )
+}
+
+with open("models/metricas.json", "w") as f:
+    json.dump(metricas, f, indent=4)
+
+centroides_original = scaler.inverse_transform(kmeans.cluster_centers_)
+centroides_df = pd.DataFrame(
+    centroides_original,
+    columns=data.columns.drop(["id_cliente", "cluster", "pc1", "pc2"])
+)
+centroides_df.to_csv("data/centroides.csv", index=False)
+
+pickle.dump(kmeans, open("models/modelo_kmeans.pkl", "wb"))
+pickle.dump(scaler, open("models/scaler.pkl", "wb"))
+pickle.dump(pca, open("models/pca.pkl", "wb"))
+print("Modelo guardado")
